6a22e5495df1622e10e8fc486ebd9937465711e7
yanye <yanye@apkpure.net>
2020-10-14 09:46:06 +0800

    nuxt.js

--- 08-nuxt-demo/plugins/interceptor.js
+++ 08-nuxt-demo/plugins/interceptor.js
@@ -1,6 +1,5 @@
 export default function ({$axios, store}) {
     $axios.onRequest(config =>{
-       
         if(store.state.user.token){
             config.headers.Authorization = 'Bearer' + store.state.user.token;
         }
