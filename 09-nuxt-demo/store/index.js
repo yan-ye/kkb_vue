c7c488bc44175ad8221f1d1366af6c6a64666edf
yanye <yanye@apkpure.net>
2020-10-15 10:27:03 +0800

    nuxtServerInit 添加注释

--- 09-nuxt-demo/store/index.js
+++ 09-nuxt-demo/store/index.js
@@ -11,11 +11,12 @@ export const mutations = {
 
 export const actions = {
     nuxtServerInit({ commit }, { app }) {
-        console.log(app, '>>>>')
-        // const token = app.$cookise.get('token')
-        // if(token){
-        //     console.log('nuxtServerInit: token:' + token)
-        //     commit("user/init", token);
-        // }
+        //当我们想将服务端的一些数据传到客户端时，这个方法非常好用
+        //app 是server的实例  也就是express 的实例
+        const token = app.$cookies.get('token')
+        if(token){
+            console.log('nuxtServerInit: token:' + token)
+            commit("user/init", token);
+        }
     }
 }
